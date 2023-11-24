dc3bfea8320d65ee74bd4b0da9fa477351a1d5a8
sangkwonlee1722 <kjess8335@gmail.com>
2023-11-25 05:01:13 +0900

    feat: [dev_comment] 댓글 가져오기기능 구현

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -45,4 +45,44 @@ commentsRouter.post("/:postId/comments", token_middleware, async (req, res) => {
     }
 });
 
+// 댓글 가져오기
+commentsRouter.get("/:postId/comments", token_middleware, async (req, res) => {
+    try {
+        const { postId } = req.params;
+
+        // 게시물 확인
+        const post = await Post.findByPk(postId);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "해당 게시물을 찾을 수 없습니다.",
+            });
+        }
+
+        // 해당 게시물의 댓글 가져오기
+        const comments = await Comments.findAll({
+            where: { postId },
+        });
+
+        if (comments.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "작성되어 있는 댓글이 없습니다.",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "댓글을 성공적으로 가져왔습니다.",
+            data: comments,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "댓글을 가져오는 중에 오류가 발생했습니다.",
+        });
+    }
+});
+
 export { commentsRouter };
