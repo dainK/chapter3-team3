ef4793b94674f6f5c98f23edc72f76bc809f54b3
sangkwonlee1722 <kjess8335@gmail.com>
2023-11-25 04:59:33 +0900

    feat: [dev_comment] 댓글작성기능 구현 완료

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -1,20 +1,48 @@
-import express from "express";
+import { Router } from "express";
+import db from "../models/index.js";
 import { token_middleware } from "../middlewares/token_middleware.js";
 
-const { Comments, Users, Post, likes } = db;
+const { Comments, Users, Post } = db;
 
-const commentsRouter = express.Router();
+const commentsRouter = Router();
 
 // 댓글 생성
-commentsRouter.post;
+commentsRouter.post("/:postId/comments", token_middleware, async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const { comment } = req.body;
 
-// 댓글 가져오기
-commentsRouter.get;
+        // 게시물 존재 여부 확인
+        const post = await Post.findByPk(postId);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "해당 게시물을 찾을 수 없습니다.",
+            });
+        }
 
-// 댓글 수정
-commentsRouter.put;
+        // 여기서부터 수정
+        const userId = res.locals.user.id; // 토큰에서 가져온 유저 ID
 
-// 댓글 삭제
-commentsRouter.delete;
+        // 댓글 생성
+        const newComment = await Comments.create({
+            postId: postId,
+            userId: userId, // 위에서 가져온 유저 ID 사용
+            comment: comment,
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: "댓글이 성공적으로 생성되었습니다.",
+            data: newComment,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "댓글 생성 중에 오류가 발생했습니다.",
+        });
+    }
+});
 
 export { commentsRouter };
