5c7522c35ab37130e2e515ce03335f37898f9116
sangkwonlee1722 <kjess8335@gmail.com>
2023-11-26 02:44:52 +0900

    refactor [dev_post] 라우터 comments에서 comment로 일괄변경

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -7,46 +7,50 @@ const { Comments, Users, Post } = db;
 const commentsRouter = Router();
 
 // 댓글 작성
-commentsRouter.post("/post/:postId/comments", token_middleware, async (req, res) => {
-    try {
-        const { postId } = req.params;
-        const { comment } = req.body;
+commentsRouter.post(
+    "/post/:postId/comment",
+    token_middleware,
+    async (req, res) => {
+        try {
+            const { postId } = req.params;
+            const { comment } = req.body;
 
-        // 게시물 확인
-        const post = await Post.findByPk(postId);
-        if (!post) {
-            return res.status(404).json({
+            // 게시물 확인
+            const post = await Post.findByPk(postId);
+            if (!post) {
+                return res.status(404).json({
+                    success: false,
+                    message: "해당 게시물을 찾을 수 없습니다.",
+                });
+            }
+
+            // 또 오류나면 먼저 확인해볼 곳(토큰에서 가져온 유저 ID)
+            const userId = res.locals.user.id;
+
+            // 댓글 작성
+            const newComment = await Comments.create({
+                postId: postId,
+                userId: userId, // 다음 확인해볼 곳(위에서 가져온 유저 ID)
+                comment: comment,
+            });
+
+            return res.status(201).json({
+                success: true,
+                message: "댓글을 성공적으로 등록하였습니다.",
+                data: newComment,
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({
                 success: false,
-                message: "해당 게시물을 찾을 수 없습니다.",
+                message: "댓글 작성 중에 오류가 발생했습니다.",
             });
         }
-
-        // 또 오류나면 먼저 확인해볼 곳(토큰에서 가져온 유저 ID)
-        const userId = res.locals.user.id;
-
-        // 댓글 작성
-        const newComment = await Comments.create({
-            postId: postId,
-            userId: userId, // 다음 확인해볼 곳(위에서 가져온 유저 ID)
-            comment: comment,
-        });
-
-        return res.status(201).json({
-            success: true,
-            message: "댓글을 성공적으로 등록하였습니다.",
-            data: newComment,
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "댓글 작성 중에 오류가 발생했습니다.",
-        });
-    }
-});
+    },
+);
 
 // 댓글 불러오기
-commentsRouter.get("/post/:postId/comments",async (req, res) => {
+commentsRouter.get("/post/:postId/comment", async (req, res) => {
     try {
         const { postId } = req.params;
 
@@ -87,7 +91,7 @@ commentsRouter.get("/post/:postId/comments",async (req, res) => {
 
 // 댓글 수정
 commentsRouter.put(
-    "/post/:postId/comments/:commentId",
+    "/post/:postId/comment/:commentId",
     token_middleware,
     async (req, res) => {
         try {
@@ -151,7 +155,7 @@ commentsRouter.put(
 
 // 댓글 삭제
 commentsRouter.delete(
-    "/post/:postId/comments/:commentId",
+    "/post/:postId/comment/:commentId",
     token_middleware,
     async (req, res) => {
         try {
