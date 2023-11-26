83b73675e54041043a3864d0fe4cf19cee61b804
da <dain940209@gmail.com>
2023-11-26 18:34:50 +0900

    feat:[dev_front_post] 메인페이지 게시물 정렬 - api 요청 및 받아온 데이터 적용 - css 작업 및 front 기능 구현

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,42 +1,107 @@
-console.log("데이터테스트");
+document.addEventListener("DOMContentLoaded", function () {
+  SettingNavigation();
+  SettingPostList();
+  document.getElementById(`main-post-container`).style.display = 'block';
+  document.getElementById(`category-post-container`).style.display = 'none';
+});
 
-fetch('/api/post')
+// ______________________________카테고리____________________________________
+document.getElementById("category-posts").addEventListener("click", ()=>{
+  MovePostList("");
+});
+
+document.getElementById("category-parts").addEventListener("click", ()=>{
+  MovePostList("?categoryId=1");
+});
+
+document.getElementById("category-laptop").addEventListener("click", ()=>{
+  MovePostList("?categoryId=2");
+});
+
+document.getElementById("category-monitor").addEventListener("click", ()=>{
+  MovePostList("?categoryId=3");
+});
+
+document.getElementById("category-keyboard").addEventListener("click", ()=>{
+  MovePostList("?categoryId=4");
+});
+
+document.getElementById("category-mouse").addEventListener("click", ()=>{
+  MovePostList("?categoryId=5");
+});
+//__________________________________________________________________________
+
+// ______________________________게시물______________________________________
+document.getElementById("last-post-list").addEventListener("click", ()=>{
+  MovePostList("?sort=new");
+});
+
+document.getElementById("like-post-list").addEventListener("click", ()=>{
+  MovePostList("?sort=likes");
+});
+
+document.getElementById("comment-post-list").addEventListener("click", ()=>{
+  MovePostList("?sort=comments");
+});
+
+document.getElementById("total-post-list").addEventListener("click", ()=>{
+  MovePostList("?sort=new");
+});
+//__________________________________________________________________________
+
+function SettingNavigation() {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, { edge: "right" });
+}
+
+function SettingPostList() {
+  fetch('/api/post/main')
   .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    for (let i = 0; i < 5; i++) {
-      const post = document.getElementById(`last-post-${i}`);
-      post.innerText = data.data[i].title;
+  .then(response => {
+    console.log(response);
+
+    response.data.createdAtPosts.forEach((e,i) => {
+      document.getElementById(`last-post-${i}`).innerText = e;
+    });
+
+    response.data.likePosts.forEach((e,i) => {
+      document.getElementById(`like-post-${i}`).innerText = e;
+    });
+    
+    response.data.commentPosts.forEach((e,i) => {
+      document.getElementById(`comment-post-${i}`).innerText = e;
+    });
+    
+    response.data.totalPosts.forEach((e,i) => {
+      document.getElementById(`total-post-${i}`).innerText = e;
+    });
+  })
+  .catch(error => console.error('에러 발생:', error));
+}
+
+function MovePostList ( sortType ) {
+  fetch('/api/post/rank' + sortType)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+
+    const parentElement = document.getElementById("category-post-list");
+    let firstChild = parentElement.firstChild;
+    while (firstChild) {
+      firstChild.innerText = "";
+      firstChild = firstChild.nextSibling;
     }
-    for (let i = 0; i < 5; i++) {
-      const post = document.getElementById(`like-post-${i}`);
-      post.innerText = data.data[i].title;
-    }
-    for (let i = 0; i < 5; i++) {
-      const post = document.getElementById(`comment-post-${i}`);
-      post.innerText = data.data[i].title;
-    }
-    for (let i = 0; i < 5; i++) {
-      const post = document.getElementById(`total-post-${i}`);
-      post.innerText = data.data[i].title;
-    }
+
+    response.data.forEach((e,i) => {
+      if(i<10) { // 임시코드 수정하기
+        document.getElementById(`category-post-${i}`).innerText = e.title;
+      }
+    });
+    
+  document.getElementById(`main-post-container`).style.display = 'none';
+  document.getElementById(`category-post-container`).style.display = 'block';
 
   })
   .catch(error => console.error('에러 발생:', error));
 
-fetch('/api/user/me')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.data)
-    const nickName = document.getElementById('userNickName')
-    nickName.innerText = data.data.nickName
-    const userImage = document.getElementById('userProfileImage')
-    userImage.src = data.data.imgUrl
-
-
-  }).catch(error => console.log(error))
-
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, { edge: "right" });
-});
+}
